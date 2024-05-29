10f93100a6b0d224cc67afd5144672dfe543ab8a
Awalhossain <waqin21@gmail.com>
2024-05-29 23:22:20 +0600

    refactor: Update Comment component to display email only on larger screens

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -9,14 +9,14 @@ interface CommentProps {
 export default function Comment({ name, email, body }: CommentProps) {
     return (
         <div className={` bg-gray-100 p-4 rounded-lg mb-2`}>
-            <div className="flex items-center mb-2">
+            <div className="block sm:flex items-center mb-2">
                 <div className="bg-gray-400 rounded-full w-8 h-8 flex items-center justify-center text-white font-bold mr-2">
                     {name.charAt(0).toUpperCase()}
                 </div>
                 <p className="font-bold">
                     {name.split(" ")[0]}
                 </p>
-                <p className="text-gray-500 ml-2">({email})</p>
+                <p className="text-gray-500 ml-2 hidden sm:block">({email})</p>
             </div>
             <p>{body}</p>
         </div>
