25dbad72903a1bf252df4eb45b908ff8c16c0447
Awalhossain <waqin21@gmail.com>
2024-05-29 22:16:21 +0600

    feat: Add UserType definition for users 🙋‍♂️

--- src/types/post.types.ts
+++ src/types/post.types.ts
@@ -6,3 +6,59 @@ export type CommentType = {
     email: string;
     body: string;
 };
+
+
+
+export type PostType = {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+};
+
+// "id": 1,
+//         "name": "Leanne Graham",
+//         "username": "Bret",
+//         "email": "Sincere@april.biz",
+//         "address": {
+//             "street": "Kulas Light",
+//             "suite": "Apt. 556",
+//             "city": "Gwenborough",
+//             "zipcode": "92998-3874",
+//             "geo": {
+//                 "lat": "-37.3159",
+//                 "lng": "81.1496"
+//             }
+//         },
+//         "phone": "1-770-736-8031 x56442",
+//         "website": "hildegard.org",
+//         "company": {
+//             "name": "Romaguera-Crona",
+//             "catchPhrase": "Multi-layered client-server neural-net",
+//             "bs": "harness real-time e-markets"
+//         }
+
+export type UserType = {
+    id: number;
+    name: string;
+    email: string;
+    username: string;
+    phone: string;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    };
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: {
+            lat: string;
+            lng: string;
+        };
+    };
+    
+};
