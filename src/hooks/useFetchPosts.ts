0cf2c61a66836d5869d9c21ded3abf53538483e9
Awalhossain <waqin21@gmail.com>
2024-05-30 00:16:08 +0600

    refactor: Create useFetchPosts hook for fetching posts in useFetchPosts.ts

---
+++ src/hooks/useFetchPosts.ts
@@ -0,0 +1,31 @@
+import { fetchPosts } from "@/services/postService";
+import { PostType } from "@/types/post.types";
+import { useEffect, useState } from "react";
+
+
+const useFetchPosts = () => {
+    const [posts, setPosts] = useState<PostType[]>([]);
+    const [error, setError] = useState(false);
+    const [isLoadingPosts, setIsLoadingPosts] = useState(true);
+
+        
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const postsData = await fetchPosts();
+                setPosts(postsData);
+                setIsLoadingPosts(false);
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+                setError(true);
+                setIsLoadingPosts(false);
+            }
+        }
+        fetchData();
+        },[]);
+
+        return { posts, error, isLoadingPosts };
+}
+
+export default useFetchPosts;
