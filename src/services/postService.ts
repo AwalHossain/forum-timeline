585406588da6afa9705fecfbfa7478f28cc43890
Awalhossain <waqin21@gmail.com>
2024-05-30 11:40:33 +0600

    refactor: Update fetchPosts to include pagination with pageParam

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -2,12 +2,14 @@ import { PostType } from '@/types/post.types';
 import { apiService } from './apiService';
 
 
+export const MAX_POST_LIMIT = 10;
 
-export const fetchPosts = async () => {
+
+export const fetchPosts = async ({pageParam}:{pageParam:number}) => {
 
 
   try {
-    const response = await apiService.get<PostType[]>(`/posts`);
+    const response = await apiService.get<PostType[]>(`/posts?_page=${pageParam}&_limit=${MAX_POST_LIMIT}`);
     return response;
   } catch (error) {
     console.error('Error fetching posts:', error);
